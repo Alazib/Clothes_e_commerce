2e8a10a814e96a57669567feabc8a2690497b00b
J.Alberto Azevedo <alazib7@gmail.com>
2023-08-10 17:58:06 +0200

    Style with styled-components the CartDropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,8 @@
-import "./cart-dropdown.styles.scss"
+import {
+  CartDropdownContainer,
+  CartItems,
+  EmptyMessage,
+} from "./cart-dropdown.styles.jsx"
 import Button from "../../components/button/button.component"
 import CartItem from "../cart-item/cart-item.component"
 import { useContext } from "react"
@@ -14,19 +18,19 @@ const CartDropdown = () => {
   }
 
   return (
-    <div className="cart-dropdown-container">
-      <div className="cart-items">
+    <CartDropdownContainer>
+      <CartItems>
         {cartProducts.length ? (
           cartProducts.map((product) => (
             <CartItem product={product} key={product.id}></CartItem>
           ))
         ) : (
-          <span>Your cart is empty</span>
+          <EmptyMessage>Your cart is empty</EmptyMessage>
         )}
-      </div>
+      </CartItems>
 
       <Button onClick={goToCheckout}>GO TO CHECKOUT</Button>
-    </div>
+    </CartDropdownContainer>
   )
 }
 
