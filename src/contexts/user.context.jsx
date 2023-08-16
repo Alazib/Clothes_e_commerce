e44acd480d5173cd390e552b6f93fbec653d4816
J.Alberto Azevedo <alazib7@gmail.com>
2023-08-16 14:32:01 +0200

    Revert "Move from useState to useReducer in user.context"
    This reverts commit 8b8825179d7742d56d779bec6f2373139fc234e8.

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useReducer } from "react"
+import { createContext, useState, useEffect } from "react"
 import {
   createUserDocumentFromAuth,
   onAuthStateChangedListener,
@@ -11,38 +11,9 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 })
 
-export const USER_ACTION_TYPES = {
-  SET_CURRENT_USER: "SET_CURRENT USER",
-}
-
-const userReducer = (state, action) => {
-  const { type, payload } = action
-
-  switch (type) {
-    case USER_ACTION_TYPES.SET_CURRENT_USER:
-      return {
-        ...state,
-        currentUser: payload,
-      }
-
-    default:
-      throw new Error(`Unhandled type ${type} in userReducer`)
-  }
-}
-
-const INITIAL_STATE = {
-  currentUser: null,
-}
-
 //the component will wrap around any other component that needs access to user context
 export const UserProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(userReducer, INITIAL_STATE)
-  const { currentUser } = state
-
-  const setCurrentUser = (user) => {
-    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user })
-  }
-
+  const [currentUser, setCurrentUser] = useState(null)
   const value = { currentUser, setCurrentUser }
 
   useEffect(() => {
