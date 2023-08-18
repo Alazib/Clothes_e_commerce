b23063df3d5775db7dc5cefd9a57037b831e4ddf
J.Alberto Azevedo <alazib7@gmail.com>
2023-08-18 14:01:57 +0200

    Migrate to Redux(4): Get the values in Redux. User

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom"
 import { useContext } from "react"
-import { UserContext } from "../../contexts/user.context"
+
 import {
   NavigationContainer,
   LogoContainer,
@@ -12,9 +12,12 @@ import { signOutUser } from "../../utils/firebase/firebase.utils"
 import CartIcon from "../../components/cart-icon/cart-icon.component"
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component"
 import { CartContext } from "../../contexts/cart.context"
+import { useSelector } from "react-redux"
+import { selectCurrentUser } from "../../store/user/user-selector.js"
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext)
+  const currentUser = useSelector(selectCurrentUser)
+
   const { isCartOpen } = useContext(CartContext)
 
   return (
