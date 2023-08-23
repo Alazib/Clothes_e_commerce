de5711734825853b4bdcb979540af4c55e47d78b
J.Alberto Azevedo <alazib7@gmail.com>
2023-08-23 19:42:07 +0200

    Add Redux persistence

--- src/store/store.js
+++ src/store/store.js
@@ -4,10 +4,27 @@ import {
   applyMiddleware,
 } from "redux"
 import logger from "redux-logger"
-
 import { rootReducer } from "./root-reducer"
+import { persistStore, persistReducer } from "redux-persist"
+import storage from "redux-persist/lib/storage"
+
+/// Setting the persistence of the Redux store
+
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+///
+
+/// Setting the Redux store
 
 const middleWares = [logger] //This middleware loggs out the state of the store before and after the action hits the reducers
 const composedEnhancers = compose(applyMiddleware(...middleWares))
 
-export const store = createStore(rootReducer, undefined, composedEnhancers)
+export const store = createStore(persistedReducer, undefined, composedEnhancers)
+export const persistor = persistStore(store)
+///
